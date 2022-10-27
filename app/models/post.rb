5455b7f429d65da591efd27c8dabdd8c8707ea15
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-10-28 02:19:10 +0300

    count the posts of the user

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :comments
     has_many :likes
+    after_save :update_posts_counter
+
+    private
+    def update_posts_counter
+        user.update(posts_counter :user.posts.length)
+    end
 end
