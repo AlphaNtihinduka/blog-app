7de11257d34a9bfc0c833bde3e69b73e94ad700a
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-10-28 20:40:59 +0300

    Validate user attributes

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
     subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
-    before { subjec.save }
+    before { subject.save }
 
     context 'Test the user validations' do
         it 'name should be present' do
             subject.name = nil
 
-            expect(subject).to_not_be_valid
+            expect(subject).to_not be_valid
         end
 
     end
