4be42eadd0cb1c543fa665115ed7066517fced57
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-01 22:15:16 +0300

    Show one user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
     def index
         @users = User.all
     end
+
+    def show
+        @user = User.find(params[:id])
+        @posts = @user.recent_three_posts
+    end
 end
