306798687656bf8dfbaf5dca8bbfbae8c60e53d6
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-03 17:07:25 +0300

    Remove the user and post collections

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,9 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
-    @posts = Post.where(author_id: @user.id)
+
   end
 
   def show
-    @user = User.find(params[:user_id])
-    @post = Post.find(params[:user_id])
-    @comments = Comment.where(post_id: @post.id)
+
   end
 end
