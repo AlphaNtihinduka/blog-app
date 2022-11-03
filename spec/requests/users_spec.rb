ca49f6d7a787dba66d6c42b1abdc79f4a36fdf80
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-03 16:39:00 +0300

    Rename the controllers to requests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,15 +1,18 @@
 require "rails_helper"
 
-RSpec.describe UsersController, :type => :controller do
-
+RSpec.describe 'Users', type: :request do
   describe "GET #index" do
-    before(:example) { get :index}
+    before(:example) { get '/'}
     it "returns a 200 OK status" do
       expect(response).to have_http_status(:ok)
     end
 
     it 'Is correct template rendered' do
       expect(response).to render_template('index')
+    end
+
+    it 'Is the body includes correct text' do
+      expect(response.body).to include('This is a list of all users')
     end
   end
 
