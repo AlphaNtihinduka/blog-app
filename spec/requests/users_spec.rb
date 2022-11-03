0e6aa70342f32655e0e667f2aa9111247a8f7170
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-03 16:46:28 +0300

    Get users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe 'Users', type: :request do
   describe "GET #index" do
-    before(:example) { get '/'}
+    before(:example) { get '/users'}
     it "returns a 200 OK status" do
       expect(response).to have_http_status(:ok)
     end
@@ -15,5 +15,20 @@ RSpec.describe 'Users', type: :request do
       expect(response.body).to include('This is a list of all users')
     end
   end
+
+  describe "GET #show" do
+  before(:example) { get '/users/:id'}
+  it "returns a 200 OK status" do
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'Is correct template rendered' do
+    expect(response).to render_template('show')
+  end
+
+  it 'Is the body includes correct text' do
+    expect(response.body).to include('Listing all the Posts')
+  end
+end
 
 end
