71d68712c8470ae9f8cb79b7dd3870146e777617
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-03 17:09:43 +0300

    Test the show controller for the posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -15,4 +15,19 @@ RSpec.describe 'Posts', type: :request do
       expect(response.body).to include('Posts for selected user')
     end
   end
+
+  describe "GET #show" do
+  before(:example) { get '/users/:id/posts/:id'}
+  it "returns a 200 OK status" do
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'Is correct template rendered' do
+    expect(response).to render_template('show')
+  end
+
+  it 'Is the body includes correct text' do
+    expect(response.body).to include('All comments for the post')
+  end
+end
 end
