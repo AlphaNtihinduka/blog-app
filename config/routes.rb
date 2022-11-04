09a768fb6e4a13b182f4187786a878f3fd6f64d9
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-04 20:37:55 +0300

    Add new route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show new]
   end
 end
