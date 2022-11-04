7b8984ac0dce542c3aaff7d778f774cb6567cf58
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-04 22:00:40 +0300

    Save a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,4 +13,22 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+
+    if @post.save
+      redirect_to user_path(current_user.id)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
