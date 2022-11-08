922b1e7e97068eed9b5d4b9ad1d66e946c902432
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-09 01:55:37 +0300

    Fix linter errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET #index' do
-    before(:each) { 
+    before(:each) do
       User.create(
         name: 'Alpha Ijeoma',
         photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
@@ -10,9 +10,8 @@ RSpec.describe 'Posts', type: :request do
         posts_counter: 0
       )
 
-    get users_path(1)
-  
-  }
+      get users_path(1)
+    end
     it 'returns a 200 OK status' do
       expect(response).to have_http_status(:ok)
     end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET #index' do
-    before(:example) { 
+    before(:example) do
       User.create(
         name: 'Alpha Ijeoma',
         photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
         bio: 'This user has been a collaborative creation',
         posts_counter: 0
       )
-    get '/users' 
-  }
+      get '/users'
+    end
     it 'returns a 200 OK status' do
       expect(response).to have_http_status(:ok)
     end
@@ -25,10 +25,9 @@ RSpec.describe 'Users', type: :request do
   end
 
   describe 'GET #show' do
-    before(:each) { 
-    get users_path(1)
-  
-  }
+    before(:each) do
+      get users_path(1)
+    end
     it 'returns a 200 OK status' do
       expect(response).to have_http_status(:ok)
     end
