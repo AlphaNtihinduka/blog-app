aa8aee13b94cce6724deb7c5ca55bbfbb7d66f31
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-09 00:34:41 +0300

    Add new post show view tests

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -1,51 +1,34 @@
 require 'rails_helper'
-
 RSpec.describe 'User pages', type: :system do
-
   before(:all) do
     Rails.application.load_seed
   end
-
   after(:all) do
     Like.destroy_all
     Comment.destroy_all
     Post.destroy_all
     User.destroy_all
-  end 
-
+  end
     describe 'show page' do
         let(:author) { User.first.id }
         subject(:post) { Post.first.id }
         before(:each) do
           visit user_post_path(author, post)
         end
-
-      it 'shows first username' do
+      it 'shows first Like button' do
         expect(page).to have_content('Like')
-      end     
-      
-        it 'shows number of posts for the username' do
-        expect(page).to have_content('Number of posts: 3')
       end
-
-      it 'shows number of comments and likes for the first post' do
-        expect(page).to have_content('comments: 2 likes: 0')
+      it 'shows first Like button' do
+        expect(page).to have_content('Comment')
       end
-
-       it 'shows create new post button' do
-        expect(page).to have_content('Create new post')
+      it 'shows the comments counter' do
+        expect(page).to have_content('Comments counter: 2')
       end
-
-      it 'shows the profile picture of the user' do
-        image = page.all('img')
-        expect(image.size).to eq(1)
+      it 'shows the likes counter' do
+        expect(page).to have_content('Likes_counter: 0')
       end
-
-      it 'redirects to the posts page when clicking on the username' do
-        click_link('See all posts')
-        expect(page).to have_content('Bio')
+      it 'shows create new post button' do
+        expect(page).to have_content('Tom: Hi Elvis!')
       end
-
-    
     end
   end
