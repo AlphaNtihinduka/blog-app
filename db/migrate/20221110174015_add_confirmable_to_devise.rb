3b1b46c5941010cf9a71a2a2807071eae2209e8e
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-10 21:49:07 +0300

    Fix linter errors

--- db/migrate/20221110174015_add_confirmable_to_devise.rb
+++ db/migrate/20221110174015_add_confirmable_to_devise.rb
@@ -1,14 +1,15 @@
 class AddConfirmableToDevise < ActiveRecord::Migration[7.0]
   def up
     add_column :users, :confirmation_token, :string
-    add_column :users, :confirmed_at,       :datetime
-    add_column :users, :confirmation_sent_at , :datetime
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
     add_column :users, :unconfirmed_email, :string
 
-    add_index  :users, :confirmation_token, :unique => true
+    add_index :users, :confirmation_token, unique: true
   end
+
   def down
-    remove_index  :users, :confirmation_token
+    remove_index :users, :confirmation_token
 
     remove_column :users, :unconfirmed_email
     remove_column :users, :confirmation_sent_at
