43c96476c5d56d70e16d468df06f5eb805aea8a0
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-10 13:08:33 +0300

    Remove photo and bio parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ class ApplicationController < ActionController::Base
   
     def update_allowed_parameters
       devise_parameter_sanitizer.permit(:sign_up) do |u|
-        u.permit(:name, :photo, :bio, :email, :password, :password_confirmation)
+        u.permit(:name, :email, :password, :password_confirmation)
       end
       devise_parameter_sanitizer.permit(:account_update) do |u|
-        u.permit(:name, :photo, :bio, :email, :password, :current_password)
+        u.permit(:name, :email, :password, :current_password)
       end
     end
 end
