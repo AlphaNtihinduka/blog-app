cfd070b9429c71fe2f33465d9f0df3367f635491
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-10 12:26:42 +0300

    Add user modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  validates :name, :photo, :bio, presence: true, length: { maximum: 200 }
+  validates :name, presence: true, length: { maximum: 200 }
   validates_numericality_of :posts_counter, only_integer: true, greater_than_or_equal: 0
 
   def recent_three_posts
