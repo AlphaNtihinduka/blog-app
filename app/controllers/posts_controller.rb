3ab8cfe41d645a464ec2dcde3afe3284702b06c6
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-11 12:29:23 +0300

    Add post authorization and destroy method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
   def index
     @user = User.find(params[:user_id])
     @posts = Post.where(author_id: @user.id).includes(:comments)
@@ -24,6 +25,14 @@ class PostsController < ApplicationController
       redirect_to user_path(current_user.id)
     else
       render :new
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id]).destroy
+
+    respond_to do |format|
+      format.html { redirect_to user_path(current_user), notice: 'Deleted!' }
     end
   end
 
