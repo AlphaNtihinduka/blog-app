86864a6bb728c2341e01a33da0ddbf3b9a2f9c2a
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-11 15:05:42 +0300

    Add dependence destruction on like

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
-  has_many :likes
+  has_many :likes, dependent: :destroy
   after_save :update_posts_counter
+  after_destroy :update_counters
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :text, presence: true
@@ -14,6 +15,10 @@ class Post < ApplicationRecord
   end
 
   private
+
+  def update_counters
+    author.update(posts_counter: author.posts.count)
+  end 
 
   def update_posts_counter
     author.increment!(:posts_counter)
