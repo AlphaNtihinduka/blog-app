74dd12282abc58a00cff14c780a359cbe28bda1c
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-11 15:07:45 +0300

    Update comment create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,13 @@
 class CommentsController < ApplicationController
   load_and_authorize_resource
   def create
-    @user = Post.find(params[:user_id])
-    @post = Post.find(params[:post_id])
     @comment = Comment.new(comment_params)
-    @comment.author = current_user
-    @comment.post = @post
+    @comment.author_id = params[:user_id]
+    @comment.post_id = params[:post_id]
+  
 
     if @comment.save
-      redirect_to user_post_path(@user, @post)
+      redirect_to user_post_path(@comment.author_id, @comment.post_id)
     else
       render :new
     end
