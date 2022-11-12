8d339a4240439ae1842ca738fe0239f694710fec
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-12 15:28:33 +0300

    Adding the api routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,15 @@ Rails.application.routes.draw do
       resources :likes, only: %i[create]
     end
   end
+
+  # API ROUTES
+  namespace :api do # /api
+    namespace :v1 do # /api/v1
+      resources :users do # api/v1/users
+        resources :posts, format: :json do # api/v1/users/:user_id/posts
+          resources :comments, format: :json # api/v1/users/:user_id/posts/:post_id/comments
+        end
+      end
+    end
+  end
 end
